0af3112161bc316beb16aa68d397bf51766541d3
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2023-12-24 03:53:53 -0500

    Add files via upload

---
+++ soundfontconverter/src/test/java/com/example/soundfontconverter/SoundfontconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soundfontconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundfontconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
